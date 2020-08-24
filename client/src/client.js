5683a643b89801fbac3b257c44c18a914297322e
jeo19 <terrakkk@outlook.com>
2020-08-24 22:02:45 +0800

    react:Delete the client/src/index.js for study a project

--- client/src/client.js
+++ client/src/client.js
@@ -2,10 +2,16 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
-const link = new HttpLink({ uri: 'https://localhost:4000/' })
+/**
+ * Create an Apollo Client, Link it to an API,
+ *  and export it as a default client
+ */
+
+const link = new HttpLink({ uri: "http://localhost:4000/" });
 const cache = new InMemoryCache()
 const client = new ApolloClient({
   link,
   cache
 })
+
 export default client
